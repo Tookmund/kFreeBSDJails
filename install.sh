5841e3d00e0cb83a9f3638a49ab397695e1d6199
Jacob Adams <tookmund@gmail.com>
2015-01-10 23:07:41 -0500

    use saner defaults

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 # Install scripts to their install locations, as configured by conf.sh
-. /srv/jail/scripts/conf.sh
-SCRIPTS=(new.sh conf.sh jexec mkjail.sh rule4.sh run.sh)
+
+. ./conf.jail
+if [ ! `id -u` = 0 ]
+then
+	echo "Install must be done as root"
+	exit 0
+fi
+SCRIPTS=(genscripts.jail.sh mk.jail.sh rule4.sh run.sh)
 install -m 700 -g root -o root -t $LOC $SCRIPTS
 
 # Create directories for runscripts and jails
