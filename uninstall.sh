a3591395286efc00daad85e030b4bdbd210d96d8
Jacob Adams <tookmund@gmail.com>
2015-02-07 20:53:06 -0500

    Install and uninstall newjail and deljail

--- uninstall.sh
+++ uninstall.sh
@@ -8,7 +8,7 @@ then
 	exit 0
 fi
 
-SCRIPTS="genscripts.jail.sh mk.jail.sh rule4.sh run.sh"
+SCRIPTS="genscripts.jail.sh mk.jail.sh rule4.sh run.sh newjail deljail"
 for i in $SCRIPTS
 do
 	rm -f $LOC/$i
