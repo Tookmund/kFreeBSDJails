a429b8ac03cb6307e0cdd78a21b87b9a5cc60506
Jacob Adams <tookmund@gmail.com>
2015-07-23 16:42:44 -0400

    Add basejail script

---
+++ basejail.sh
@@ -0,0 +1,32 @@
+# newjail $BASE $NAME $IP
+BASE=$1
+NAME=$2
+IP=$3
+HOSTNAME=$NAME.$DN
+
+if [ -e $JAILS/$NAME ]
+then
+	echo "WARNING: Jail $NAME already exists!"
+	echo "Overwrite? [y/n]"
+	read RESP
+	if [ $RESP = n ]
+	then
+		echo "Not overwriting $NAME"
+		exit 0
+	fi
+	if [ $RESP = y ]
+	then
+		echo "Overwriting $NAME"
+	else
+		echo "Invalid. Not overwriting $NAME"
+		exit 0
+	fi
+fi
+
+cp -rvf $LOC/$BASE $LOC/$NAME
+
+. $LOC/genscripts.jail.sh
+genrun
+
+echo "Done!"
+
