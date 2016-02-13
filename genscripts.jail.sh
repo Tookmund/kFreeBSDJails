4e21ff80795dd9bb0a39e2efaf8068d54253ff5c
Jacob Adams <tookmund@gmail.com>
2016-02-13 18:22:51 -0500

    genscripts: Echo config into runscript

--- genscripts.jail.sh
+++ genscripts.jail.sh
@@ -4,7 +4,7 @@ genrun () {
 	echo "Generating run script..."
 
 	echo "#!/bin/sh" > /tmp/jail.${NAME}
-	echo ". ${CONF}"
+	echo ". ${CONF}" >> /tmp/jail.${NAME}
 	echo "ifconfig ${IF} add ${IP}" >> /tmp/jail.${NAME}
 	echo "NAME=${NAME}" >> /tmp/jail.${NAME}
 	echo "IP=${IP}" >> /tmp/jail.${NAME}
