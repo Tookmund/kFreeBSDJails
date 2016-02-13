1b690c5ef68a9f315b917c141daddb02b2e7d1cf
Jacob Adams <Tookmund@users.noreply.github.com>
2016-02-13 16:21:22 -0500

    genscripts.jail.sh: Make runscripts source config file

--- genscripts.jail.sh
+++ genscripts.jail.sh
@@ -4,6 +4,7 @@ genrun () {
 	echo "Generating run script..."
 
 	echo "#!/bin/sh" > /tmp/jail.$NAME
+	echo ". $CONF jail.config"
 	echo "ifconfig $IF add $IP" >> /tmp/jail.$NAME
 	echo "NAME=$NAME" >> /tmp/jail.$NAME
 	echo "IP=$IP" >> /tmp/jail.$NAME
