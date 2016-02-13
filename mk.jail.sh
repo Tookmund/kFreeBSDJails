a83460ff4615f16e05e84a28eace4d696150fe23
Jacob Adams <tookmund@gmail.com>
2016-02-13 18:16:40 -0500

    mk.jail.sh: Fix variable typo

--- mk.jail.sh
+++ mk.jail.sh
@@ -2,7 +2,7 @@
 mkjail () {
 	debootstrap \
 	 --exclude=devd,dmidecode,isc-dhcp-client,isc-dhcp-common,kldutils,pf,vidcontrol \
-	 ${VERSION} /${JAILS}/${NAME} "{$MIRROR}"
+	 ${VERSION} /${JAILS}/${NAME} "${MIRROR}"
 
 	echo "${HOSTNAME}" > /${JAILS}/${NAME}/etc/hostname
 	echo "${IP} ${HOSTNAME}" >> /${JAILS}/${NAME}/etc/hosts
